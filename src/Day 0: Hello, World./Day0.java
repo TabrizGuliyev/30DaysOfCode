6d2499fe6099e3e838d5980a1170af7a7867cde2
TabrizGuliyev <tabrizguliyev@hotmail.com>
2018-10-23 17:30:38 +0400

    Day 0: Hello, World.

---
+++ src/Day 0: Hello, World./Day0.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author tabrizguliyev
+ */
+public class Day0 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine();
+        scan.close();
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+    }
+}
